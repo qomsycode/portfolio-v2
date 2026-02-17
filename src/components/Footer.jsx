4a68e45c083a8dc3e11b99859de959353b14f3e3
qomsycode <abdullateefqoyum02@gmail.com>
2026-02-17 12:37:13 +0100

    Simplify footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='w-full py-8 text-center bg-[#020617] text-slate-500 border-t border-slate-900 text-sm'>
-            <p>&copy; {new Date().getFullYear()} Abdullateef Qoyum (Qomzyy). Built with React & Tailwind.</p>
+            <p>&copy; {new Date().getFullYear()} Abdullateef Qoyum (Qomzyy). All rights reserved.</p>
         </div>
     );
 };
